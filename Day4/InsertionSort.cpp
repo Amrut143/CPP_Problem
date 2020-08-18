332cef0ed742f93623b7f9f167ff3f1c189abf12
Amrut <amrutpanda7789@gmail.com>
2020-08-18 12:45:26 +0530

    ADDED: Code to sort the array of element using insertion sort

---
+++ Day4/InsertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int [], int);
+
+int main() {
+
+	int arr[] = {34, 45, 11, 89, 55, 19, 22, 8, 40};
+
+	insertionSort(arr, 9);
+	cout << "After sorting the elements: " << endl;
+	for(int i = 0; i<=8; i++)
+			cout << arr[i] << endl;
+}
+
+void insertionSort(int arr[], int numOfElements) {
+
+	int i, j, temp;
+
+	for(i = 1; i < numOfElements; i++) {
+			temp = arr[i];
+			for(j = i-1; j >= 0 && temp < arr[j]; j--) {
+					arr[j+1] = arr[j];
+			}
+			arr[j+1] = temp;
+	}
+}
